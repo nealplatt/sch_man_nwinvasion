f7a30cfaf1f3f2807294eee37b1ea4f47d173ade
Neal Platt <neal.platt@gmail.com>
2020-04-02 12:24:41 -0500

    adding to code to run msprime in parallel

--- code/msprime-parallel.py
+++ code/msprime-parallel.py
@@ -1,7 +1,8 @@
-mport msprime
+import msprime
 from tqdm import tqdm                                                                                                                                 
 import os
 import subprocess
+import multiprocessing
 
 #################################
 def sim_tree(iteration, pop, length, recomb_rate, mut_rate, seed):
@@ -13,10 +14,10 @@ def sim_tree(iteration, pop, length, recomb_rate, mut_rate, seed):
         random_seed=12345)
 
     tree = tree_sequence.first()
-    print("iter {}: {} completed".format(i, pop))
+    print("iter {}: {} completed".format(iteration, pop))
     #print(tree.draw(format="unicode"))
 
-    with open("results/sch_man_nwinvasion/msprime/{}/chr1_{}_rep_{}.vcf".format(pop, pop, i), "w") as vcf_file:
+    with open("results/sch_man_nwinvasion/msprime/{}/chr1_{}_rep_{}.vcf".format(pop, pop, iteration), "w") as vcf_file:
         tree_sequence.write_vcf(vcf_file, ploidy=2)
 ########################################
 
@@ -49,31 +50,31 @@ with multiprocessing.Pool(processes=11) as pool:
 # extract only "probed" regions from the simuluated chromosomes/vcfs
 
 #print new genome index file 
-with open("results/sch_man_nwinvasion/msprime/sim_chr1.genome", 'w') as bed_genome:
-    bed_genome.write("1\t1\t8890000")
+#with open("results/sch_man_nwinvasion/msprime/sim_chr1.genome", 'w') as bed_genome:
+#    bed_genome.write("1\t1\t8890000")
 
 #now create bed for the new "sim" chr1
-with open('data/renamed-sma_agilent_baits.v7.0.chr_reorderd.bed', 'r') as in_bed:
-    with open('results/sch_man_nwinvasion/msprime/sim_probes.bed', 'w') as out_bed:
-        for bed_entry in in_bed:
-            chrom, start, stop = bed_entry.rstrip().split("\t")
-            if chrom == "SM_V7_1":
-                out_bed.write("1\t{}\t{}\n".format(start, stop))
+#with open('data/renamed-sma_agilent_baits.v7.0.chr_reorderd.bed', 'r') as in_bed:
+#    with open('results/sch_man_nwinvasion/msprime/sim_probes.bed', 'w') as out_bed:
+#        for bed_entry in in_bed:
+#            chrom, start, stop = bed_entry.rstrip().split("\t")
+#            if chrom == "SM_V7_1":
+#                out_bed.write("1\t{}\t{}\n".format(start, stop))
 
 #now loop through all of the sim vcf files to get snps at probed regions
-for pop in ["new_world", "east_africa", "west_africa"]:
-    out_dir = "results/msprime/{}".format(pop)
-    for i in tqdm(range(0, 11), desc="{} intersect".format(pop):
-
-        sim_vcf = "results/msprime/new_world/chr1_{}_rep_{}.vcf".format(pop, i)
-        int_vcf = "results/msprime/new_world/chr1_probed_{}_rep_{}.vcf".format(pop, i)
-        bed = 'results/sch_man_nwinvasion/msprime/sim_probes.bed'
-
-        vcf_cmd = "vcftools --vcf {} --bed {} --recode --recode-INFO-all --stdout >{}".format(sim_vcf, bed, int_vcf)
-
-        #run vcf cmd
-        process = subprocess.Popen(vcf_cmd.split(""),
-                             stdout=subprocess.PIPE, 
-                             stderr=subprocess.PIPE)
+#for pop in ["new_world", "east_africa", "west_africa"]:
+#    out_dir = "results/msprime/{}".format(pop)
+#    for i in tqdm(range(0, 11), desc="{} intersect".format(pop):
+#
+#        sim_vcf = "results/msprime/new_world/chr1_{}_rep_{}.vcf".format(pop, i)
+#        int_vcf = "results/msprime/new_world/chr1_probed_{}_rep_{}.vcf".format(pop, i)
+#        bed = 'results/sch_man_nwinvasion/msprime/sim_probes.bed'
+#
+#        vcf_cmd = "vcftools --vcf {} --bed {} --recode --recode-INFO-all --stdout >{}".format(sim_vcf, bed, int_vcf)
+#
+#        #run vcf cmd
+#        process = subprocess.Popen(vcf_cmd.split(""),
+#                             stdout=subprocess.PIPE, 
+#                             stderr=subprocess.PIPE)
 
 
