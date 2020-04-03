e152c465905123143ad70fbb11d2ad90822f4d4e
Neal Platt <neal.platt@gmail.com>
2020-04-03 15:41:19 -0500

    initial commit of shell workflow for msprime

---
+++ code/msprime.sh
@@ -0,0 +1,19 @@
+conda activate sch_man_nwinvasion-msprime
+cd ~/sch_man_nwinvasion
+
+mkdir -P results/msprime/logs
+
+CONDA="conda activate sch_man_nwinvasion-msprime;"
+QSUB="qsub -V -cwd -S /bin/bash -q all.q -j y -pe smp 3 "    
+
+for POP in new_world east_africa west_africa; do
+    mkdir results/msprime/$POP
+    for I in $(seq 1 342); do
+        MSPRIME_CMD="python code/msprime_qsub.py $POP $I"       
+        echo "$CONDA $MSPRIME_CMD" | $QSUB -N $POP"_"$I -o results/msprime/logs/$POP"_"$I.log
+    done
+done
+
+#now gegt the vcfs
+python code/probe_snps_from_vcf.py
+
