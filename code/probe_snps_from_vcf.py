2de98f6547483ea14ae207736dfe9cfe3f564586
Neal Platt <neal.platt@gmail.com>
2020-04-03 15:39:45 -0500

    fixed error printing out same tree (same seed number); extract probed regions

---
+++ code/probe_snps_from_vcf.py
@@ -0,0 +1,34 @@
+import msprime
+from tqdm import tqdm
+import os
+import subprocess
+import glob
+
+
+#now create bed for the new "sim" chr1
+with open('data/renamed-sma_agilent_baits.v7.0.chr_reorderd.bed', 'r') as in_bed:
+    with open('results/msprime/sim_probes.bed', 'w') as out_bed:
+        for bed_entry in in_bed:
+            chrom, start, stop = bed_entry.rstrip().split("\t")
+            if chrom == "SM_V7_1":
+                out_bed.write("1\t{}\t{}\n".format(start, stop))
+
+bed = 'results/msprime/sim_probes.bed'
+#now loop through all of the sim vcf files to get snps at probed regions
+for pop in ["new_world", "east_africa", "west_africa"]:
+    out_dir = "results/msprime/{}".format(pop)
+    
+    sim_vcfs = glob.glob("{}/chr1_*_rep_*.vcf".format(out_dir))
+    for sim_vcf in sim_vcfs:
+
+        probed_vcf = sim_vcf.replace(".vcf", "_probed.vcf")       
+        vcf_cmd = "vcftools --vcf {} --bed {} --recode --recode-INFO-all --stdout >{}".format(sim_vcf, bed, probed_vcf)
+        
+        jid = "probe_{}".format(probed_vcf.split("/")[-1])
+        log = "{}/logs".format(out_dir)
+        #run vcf cmd
+        #process = subprocess.Popen(vcf_cmd.split(""),
+        #                     stdout=subprocess.PIPE, 
+        #                     stderr=subprocess.PIPE)
+        !echo "conda actvate sch_man_nwinvasion-msprime; {vcf_cmd}" | qsub -V -cwd -S /bin/bash -q all.q -j y -pe smp 3 -N {jid} -o {log}
+
